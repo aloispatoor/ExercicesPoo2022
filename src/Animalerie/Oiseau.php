488cc3600edb1b95c0ec108c0f0ecd1cecbaa9fe
Alois <palois89@posteo.net>
2022-03-23 16:42:51 +0100

    minor adds

--- src/Animalerie/Oiseau.php
+++ src/Animalerie/Oiseau.php
@@ -6,6 +6,7 @@ namespace App\Animalerie;
 
 class Oiseau extends Animal
 {
+    private string $moyenDeLocomotion;
     public function eat(string $food): string
     {
         return "L'animal se nourrit essentiellement de {$this->food}";
