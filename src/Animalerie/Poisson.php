92934cd71ef83e4ea09d120320c49ff37273e957
Alois <palois89@posteo.net>
2022-03-28 08:53:25 +0200

    forgotten changes

--- src/Animalerie/Poisson.php
+++ src/Animalerie/Poisson.php
@@ -6,6 +6,7 @@ namespace App\Animalerie;
 
 class Poisson extends Animal
 {
+    private string $moyenDeLocomotion;
     public function eat(string $food): string
     {
         return "L'animal se nourrit essentiellement de {$this->food}";
